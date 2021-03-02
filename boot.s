1fb62530472c86acae5067e01d633739b058752a
SamuelYvon <samuel.yvon@umontreal.ca>
2021-03-02 11:39:34 -0500

    Write

--- boot.s
+++ boot.s
@@ -3,18 +3,26 @@
 # AT&T syntax
 
 .globl boot
+.globl write
+
+.code16
 
 boot:
-	.code16
+	cli
+	mov $'S, %al   
+	call write
 
-	mov $0x0e, %ah   # function number = 0Eh : Display Character
-	mov $33, %al     # AL = code of character to display
-	int $0x10        # call INT 10h, BIOS video service
-
-	jmp boot
+	hlt
 
 
+# Write a char to the screen, assumes the char is inside
+# AH
+write:
+	mov $0x0E, %ah
+	int $0x10	
+	ret
+	
 end:
-	.space (1<<9)-2-(end-boot)
+	.space 512-2-(end-boot)
 	.byte 0x55
 	.byte 0xAA
