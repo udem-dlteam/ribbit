5aed191ed2d63a8e48ab7d6ff827495e5926aed9
SamuelYvon <samuel.yvon@umontreal.ca>
2021-09-08 19:04:22 -0400

    Primes benchmark

---
+++ bench/primes.scm
@@ -0,0 +1,38 @@
+(define i-my-modulo (lambda ( a b acc)
+                      (if (< a b)
+                        a
+                        (if (< (+ acc b) a)
+                          (i-my-modulo a b (+ acc b))
+                          (if (= (+ acc b) a)
+                            0
+                            (- a acc)
+                            )))))
+
+(define my-modulo
+  (lambda (a b)
+    (i-my-modulo a b 0)))
+
+(define interval-list (lambda (m n)
+                        (if (< m n)
+                          (cons m (interval-list (+ 1 m) n))
+                          (cons m '()))))
+
+(define remove-multiples 
+  (lambda (n l)
+    (if (null? l)
+      '()
+      (if (= (my-modulo (car l) n) 0)
+        (remove-multiples n (cdr l))
+        (cons (car l)
+              (remove-multiples n (cdr l)))))))
+
+(define sieve (lambda (l)
+                (if (null? l)
+                  '()
+                  (cons (car l)
+                        (sieve (remove-multiples (car l) (cdr l)))))))
+
+(define primes<= (lambda (n)
+                   (sieve (interval-list 2 n))))
+
+(primes<= 100)
