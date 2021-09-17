ec1da9a8983ddbeebb30ef5e17461129d2df2408
Marc Feeley <feeley@iro.umontreal.ca>
2021-09-16 20:26:51 -0400

    Tweak javascript RVM

--- src/host/js/rvm.js
+++ src/host/js/rvm.js
@@ -69,20 +69,80 @@ if (nodejs) { // in nodejs? //node
   getchar = () => pos<input[lengthAttr] && push(get_byte());
 } //node
 
+// VM
+
+// build the symbol table
+
 pos = 0;
 get_byte = () => input[pos++].charCodeAt(0);
+get_code = () => { let x = get_byte()-35; return x<0 ? 57 : x; };
+get_int = (n) => { let x = get_code(); n *= 46; return x<46 ? n+x : get_int(n+x-46); };
 
-// VM
+pop = () => { let x = stack[0]; stack = stack[1]; return x; };
 
 FALSE = [0,0,5]; TRUE = [0,0,5]; NIL = [0,0,5];
 
-to_bool = (x) => x ? TRUE : FALSE;
-is_rib = (x) => x[lengthAttr];
+symtbl = NIL;
+n = get_int(0);
+while (n-- > 0) symtbl=[[0,[NIL,0,3],2],symtbl,0]; // symbols with empty names
+
+accum = NIL;
+n = 0;
+while (1) {
+  c = get_byte();
+  if (c == 44) { symtbl=[[0,[accum,n,3],2],symtbl,0]; accum = NIL; n = 0; continue; }
+  if (c == 59) break;
+  accum = [c,accum,0];
+  n++;
+}
+
+symtbl = [[0,[accum,n,3],2],symtbl,0];
+
+symbol_ref = (n) => list_tail(symtbl,n)[0];
+list_tail = (x,i) => i ? list_tail(x[1],i-1) : x;
+
+// decode the instruction graph
 
 stack = 0;
 
-push = (x) => { stack = [x,stack,0]; return true; };
-pop = () => { let x = stack[0]; stack = stack[1]; return x; };
+while (1) {
+  x = get_code();
+  n = x;
+  d = 0;
+  op = -1;
+  while ((d=[20,30,0,10,11,4][++op])+2<n) n -= d+3;
+  if (x>90)
+    n = pop();
+  else {
+    if (!op) stack = [0,stack,0];
+    n = n>=d ? (n==d ? get_int(0) : symbol_ref(get_int(n-d-1))) : op<3 ? symbol_ref(n) : n;
+    if (4<op) {
+      n = [[n,0,pop()],0,1];
+      if (!stack) break;
+      op=4;
+    }
+  }
+  stack[0] = [op?op-1:0,n,stack[0]];
+}
+
+set_global = (x) => { symtbl[0][0] = x; symtbl = symtbl[1]; };
+
+set_global([0,symtbl,1]); // primitive 0
+set_global(FALSE);
+set_global(TRUE);
+set_global(NIL);
+
+// RVM core
+
+pc = n[0][2];
+stack = [0,0,[5,0,0]]; // primordial continuation (executes halt instr.)
+
+push = (x) => ((stack = [x,stack,0]), true);
+to_bool = (x) => x ? TRUE : FALSE;
+is_rib = (x) => x[lengthAttr];
+
+get_opnd = (o) => is_rib(o) ? o : list_tail(stack,o);
+get_cont = () => { let s = stack; while (!s[2]) s = s[1]; return s; };
 
 prim1 = (f) => () => push(f(pop()));
 prim2 = (f) => () => push(f(pop(),pop()));
@@ -110,65 +170,6 @@ primitives = [
   getchar,
   prim1(putchar)
 ];
-
-get_code = () => { let x = get_byte()-35; return x<0 ? 57 : x; };
-get_int = (n) => { let x = get_code(); n *= 46; return x<46 ? n+x : get_int(n+x-46); };
-list_tail = (x,i) => i ? list_tail(x[1],i-1) : x;
-
-// build the initial symbol table
-
-symtbl = NIL;
-n = get_int(0);
-while (n-- > 0) symtbl=[[0,[NIL,0,3],2],symtbl,0];
-
-accum = NIL;
-n = 0;
-while (1) {
-  c = get_byte();
-  if (c == 44) { symtbl=[[0,[accum,n,3],2],symtbl,0]; accum = NIL; n = 0; continue; }
-  if (c == 59) break;
-  accum = [c,accum,0];
-  n++;
-}
-
-symtbl = [[0,[accum,n,3],2],symtbl,0];
-symbol_ref = (n) => list_tail(symtbl,n)[0];
-
-// decode the RVM instructions
-
-while (1) {
-  x = get_code();
-  n = x;
-  d = 0;
-  op = -1;
-  while ((d=[20,30,0,10,11,4][++op])+2<n) n -= d+3;
-  if (x>90)
-    n = pop();
-  else {
-    if (!op) stack = [0,stack,0];
-    n = n>=d ? (n==d ? get_int(0) : symbol_ref(get_int(n-d-1))) : op<3 ? symbol_ref(n) : n;
-    if (4<op) {
-      n = [[n,0,pop()],0,1];
-      if (!stack) break;
-      op=4;
-    }
-  }
-  stack[0] = [op?op-1:0,n,stack[0]];
-}
-
-pc = n[0][2];
-
-get_opnd = (o) => is_rib(o) ? o : list_tail(stack,o);
-get_cont = () => { let s = stack; while (!s[2]) s = s[1]; return s; };
-
-set_global = (x) => { symtbl[0][0] = x; symtbl = symtbl[1]; };
-
-set_global([0,symtbl,1]); // primitive 0
-set_global(FALSE);
-set_global(TRUE);
-set_global(NIL);
-
-stack = [0,0,[5,0,0]]; // primordial continuation (executes halt instr.)
 
 run = () => {
   while (1) {
