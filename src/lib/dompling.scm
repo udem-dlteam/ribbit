29947478b5170dfe5b182e4381b6bc2e9a65612c
Mathis Laroche <ecomath360@gmail.com>
2023-02-19 14:48:51 -0500

    ADD - the DOMpling library

--- src/lib/dompling.scm
+++ src/lib/dompling.scm
@@ -2,12 +2,26 @@
 
 (cond-expand
   ((host js)
-    (define-primitive (get-element-by-id id)
-      "() => push(document.getElementById(pop())"
+    (define-primitive (query-selector query)
+      (use foreign rib_to_str)
+      "() => push(foreign(document.querySelector(rib_to_str(pop()))))"
       )
-    (define-primitive (get-attr attr element)
-      "() => push(string_to_rib(pop()[rib_to_string(pop()))]"
+    (define-primitive (element-by-id id)
+      (use foreign rib_to_str)
+      "() => push(foreign(document.getElementById(rib_to_str(pop()))))"
+      )
+    (define-primitive (get-attr element attr)
+      (use foreign rib_to_str)
+      ;; If v is an object and not an array, we push it as is. Else, we convert it to a foreign rib.
+      "() => {let attr = pop(); let v = pop()[rib_to_str(attr)]; push(typeof v === 'object' && !Array.isArray(v) ? foreign(v) : any_to_rib(v))}"
+      )
+    (define-primitive (set-attr element attr-name attr-value)
+      (use any_to_rib rib_to_str)
+      ;; If v is an object and not an array, we push it as is. Else, we convert it to a foreign rib.
+      "() => {let value = pop(); let name = rib_to_str(pop()); push(typeof v === 'object' && !Array.isArray(v) ? foreign(v) : any_to_rib(v))}"
       )
     )
-
   )
+
+
+
