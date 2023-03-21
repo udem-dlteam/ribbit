e0f1212238cf876f67b5cccd8a071ae61da1ff7a
Léonard Oest O'Leary <lool4516@gmail.com>
2023-03-21 17:57:03 -0400

    Add a way to modify the rsc-compiler used in tests

--- src/makefile-common.mk
+++ src/makefile-common.mk
@@ -12,31 +12,35 @@
 # HOST_COMPILER takes two arguments, the output file (executable binary) and
 # the source file to compile
 
+RSC_COMPILER ?= ../../rsc
+
 all:
 
 build-all: build-repl-min build-repl-max build-repl-max-tc build-rsc
 
 build-repl-min: ../../repl-min.scm
-	dir="$(RIBBIT_BUILD_DIR)"; ../../rsc -t $(HOST) -l min $(RIBBIT_BUILD_OPTS) -o $${dir:-build}/repl-min.$(HOST) $<
+	dir="$(RIBBIT_BUILD_DIR)"; $(RSC_COMPILER) -t $(HOST) -l min $(RIBBIT_BUILD_OPTS) -o $${dir:-build}/repl-min.$(HOST) $<
 
 build-repl-max: ../../repl-max.scm
-	dir="$(RIBBIT_BUILD_DIR)"; ../../rsc -t $(HOST) -l max $(RIBBIT_BUILD_OPTS) -o $${dir:-build}/repl-max.$(HOST) $<
+	dir="$(RIBBIT_BUILD_DIR)"; $(RSC_COMPILER) -t $(HOST) -l max $(RIBBIT_BUILD_OPTS) -o $${dir:-build}/repl-max.$(HOST) $<
 
 build-repl-max-tc: ../../repl-max.scm
-	dir="$(RIBBIT_BUILD_DIR)"; ../../rsc -t $(HOST) -l max-tc $(RIBBIT_BUILD_OPTS) -o $${dir:-build}/repl-max-tc.$(HOST) $<
+	dir="$(RIBBIT_BUILD_DIR)"; $(RSC_COMPILER) -t $(HOST) -l max-tc $(RIBBIT_BUILD_OPTS) -o $${dir:-build}/repl-max-tc.$(HOST) $<
+	
 
-build-rsc: ../../rsc.scm
-	dir="$(RIBBIT_BUILD_DIR)"; ../../rsc -t $(HOST) -l max $(RIBBIT_BUILD_OPTS) -o $${dir:-build}/rsc.$(HOST) $<
+build-rsc: ../../rsc.scm  
+	dir="$(RIBBIT_BUILD_DIR)"; $(RSC_COMPILER) -t $(HOST) -l max $(RIBBIT_BUILD_OPTS) -o $${dir:-build}/rsc.$(HOST) $<
 
 check:
 	@host="$(HOST)"; \
 	INTERPRETER="$(HOST_INTERPRETER)"; \
 	COMPILER="$(HOST_COMPILER)"; \
+	RSC_COMPILER="${RSC_COMPILER}"; \
 	for prog in `ls ../../tests/*.scm tests/*.scm`; do \
 	  options=`sed -n -e '/;;;options:/p' $$prog | sed -e 's/^;;;options://'`; \
 	  echo "---------------------- $$prog [options:$$options]"; \
 	  rm -f test.$$host*; \
-	  ../../rsc -t $$host $$options -o test.$$host $$prog; \
+	  $$RSC_COMPILER -t $$host $$options -o test.$$host $$prog; \
 	  if [ "$$INTERPRETER" != "" ]; then \
 	    sed -n -e '/;;;input:/p' $$prog | sed -e 's/^;;;input://' | $$INTERPRETER test.$$host > test.$$host.out; \
 	  else \
