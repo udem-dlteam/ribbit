73e445ee2f71dc8de7a03bfedddac220649383f4
denheztfl <francois.luc.denhez-teuton@umontreal.ca>
2023-04-14 16:35:04 -0400

    set SHELL to 'bash' to resolve 'time not found' problem in 'make check-bootstrap'

--- src/host/rs/test_timestamped.sh
+++ src/host/rs/test_timestamped.sh
@@ -2,37 +2,46 @@
 
 timestamp="$(date +"%H:%M_%d-%m")"
 
+cd logs
+mkdir $timestamp
+cd ..
 
-BS_FILE=report_$timestamp.btsp.log
-BS_EFILE=err_warn_$timestamp.btsp.log
-FILE=report_$timestamp.log
-EFILE=err_warn_$timestamp.log
+BS_FILE=std.btsp.log
+BS_EFILE=err.btsp.log
+FILE=std.log
+EFILE=err.log
+F_FILE=std.fancy.log
+F_EFILE=err.fancy.log
 
 touch $FILE
 touch $EFILE
-touch $BS_FILE
-touch $BS_EFILE
 
-echo "Checking Bootstrap Tests...\n"
-make check-bootstrap 2> $BS_EFILE | tee $BS_FILE > /dev/null
+
 
 echo "Checking Tests...\n"
 make check 2> $EFILE | tee $FILE #> /dev/null
 
-
+mv -t logs/$timestamp $FILE $EFILE
+   
 cd ../..
 
-touch fancy_$FILE
-touch fancy_$EFILE
+touch $BS_FILE
+touch $BS_EFILE
 
-export HOST=rs
+ export HOST=rs
+
+
+echo "Checking Bootstrap Tests...\n"
+make check-bootstrap 2> $BS_EFILE | tee $BS_FILE > /dev/null
+
+mv -t host/rs/logs/$timestamp $BS_FILE $BS_EFILE
+
+touch $F_FILE
+touch $F_EFILE
+
 
 echo "Checking Fancy Tests...\n"
 make check-fancy 2> fancy_$EFILE | tee fancy_$FILE #> /dev/null
 
-mv -t host/rs/logs fancy_$FILE fancy_$EFILE
+mv -t host/rs/logs/$timestamp $F_FILE $F_EFILE
 
-cd host/rs
-
-mv -t logs $FILE $EFILE
-mv -t bs_logs $BS_FILE $BS_EFILE
