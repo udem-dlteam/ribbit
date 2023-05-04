73e7a64494ba3bfe2753a827a5b04c67a02751cf
Mathis Laroche <ecomath360@gmail.com>
2023-05-04 15:36:38 -0400

    Added a test for the io lib

---
+++ src/tests/70-open-input-file.scm
@@ -0,0 +1,31 @@
+(define data-file (open-input-file "../../tests/data/70-input-data.txt"))
+
+(display (input-port? data-file))
+(newline)
+
+(display (read-char data-file))
+(newline)
+
+(display (peek-char data-file))
+(newline)
+
+(display (read-char data-file))
+(newline)
+
+(display (eof-object? (read-char data-file)))
+(newline)
+
+(close-input-port data-file)
+(close-input-port data-file)
+
+(display (input-port-close? data-file))
+(newline)
+
+;;;options: -l min -l io 
+;;;expected:
+;;;#t
+;;;66
+;;;66
+;;;#t
+;;;#t
+;;;#t
