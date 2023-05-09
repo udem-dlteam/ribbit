8db36046a621d922ac355a316b889a8cfc0f38a1
Mathis Laroche <ecomath360@gmail.com>
2023-05-09 11:58:46 -0400

    Changed the output data file from 70-output-data to 71-output-data

--- src/tests/71-write-file.scm
+++ src/tests/71-write-file.scm
@@ -1,11 +1,11 @@
-(define data-file (open-output-file "../../tests/data/70-output-data.txt"))
+(define data-file (open-output-file "../../tests/data/71-output-data.txt"))
 
 (display (output-port? data-file))
 (newline)
 
 (write "bonjour" data-file)
 
-(call-with-input-file "../../tests/data/70-output-data.txt" 
+(call-with-input-file "../../tests/data/71-output-data.txt" 
                       (lambda (port)
                         (let loop ((ch (read-char port)))
                           (if (eof-object? ch)
@@ -18,7 +18,7 @@
 (display (output-port-close? data-file))
 (newline)
 
-;;;cleanup: rm ../../tests/data/70-output-data.txt
+;;;cleanup: rm ../../tests/data/71-output-data.txt
 ;;;options: -l io 
 ;;;expected:
 ;;;#t
