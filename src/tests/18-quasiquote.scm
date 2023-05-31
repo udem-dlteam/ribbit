f4311a8cba614d7e25588a2de3a34f81ba32eaaf
Léonard Oest O'Leary <lool4516@gmail.com>
2023-05-31 13:25:43 -0400

    add test for quasiquotes

--- src/tests/18-quasiquote.scm
+++ src/tests/18-quasiquote.scm
@@ -11,6 +11,12 @@
 (display combined-fruits)
 (newline)
 
+(display `(1 `,(+ 1 ,(+ 2 3)) 4))
+(newline)
+
+(display `(1 ```,,@,,@(list (+ 1 2)) 4))
+(newline)
+
 ;;;fancy-compiler
 ;;;options: -l r4rs/io -l r4rs/control -l r4rs/pair-list
 ;;;expected:
