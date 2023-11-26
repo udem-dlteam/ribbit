22d7acb4915bd4a411f1e92fa9a0126adb018bfb
Léonard Oest O'Leary <lool4516@gmail.com>
2023-11-26 17:05:58 -0500

    Fix test 71

--- src/tests/00-ribbit/71-write-file.scm
+++ src/tests/00-ribbit/71-write-file.scm
@@ -8,21 +8,18 @@
 
 (close-output-port data-file)
 
-(define input-port-test '())
-
 (display "File content: ")
 (call-with-input-file "tests/data/71-output-data.txt" 
                       (lambda (port)
                         (let loop ((ch (read-char port)))
                           (if (eof-object? ch)
                             (newline)
-                            (begin (write-char ch) (loop (read-char port))))
-                          (set! input-port-test port))))
+                            (begin (write-char ch) (loop (read-char port)))))))
 
 (close-output-port data-file)
 (close-output-port data-file)
 
-;;;cleanup: rm ../../tests/data/71-output-data.txt
+;;;cleanup: rm tests/data/71-output-data.txt
 ;;;options: -l r4rs/io 
 ;;;fancy-compiler
 ;;;expected:
