ceb5be8c3c83a277135a59515d0f3f542ff5a866
leo-ard <lool4516@gmail.com>
2024-01-06 19:00:58 -0500

    Fix bug with 5-2-define

--- src/tests/01-r4rs/5-2-define.scm
+++ src/tests/01-r4rs/5-2-define.scm
@@ -1,9 +1,9 @@
 (define add3 (lambda (x) (+ x 3)))
-(display (add3 3))
+(display (add3 3)) ;; 6
 (newline)
 
 (define first car)
-(display (first '(1 2)))
+(display (first '(1 2))) ;; (1 2)   
 (newline)
 
 (define old-+ +)
@@ -13,11 +13,11 @@
               (begin))
        (begin)
        (begin (begin (begin) 
-                     (display (add3 6))
+                     (display (add3 6)) ;; (3 6)
                      (newline)
                      (begin))))
 (set! + old-+)
-(display (add3 6))
+(display (add3 6)) ;; 9
 (newline)
 
 (begin)
@@ -26,20 +26,40 @@
 
 (define x 34)
 
-(display x)
+(define (foo) (define x 5) x)
+(display (foo)) ;; 5
 (newline)
+
+(display x) ;; 34
+(newline)
+
 
 (define (foo x) ((lambda () (define x 5) x)) x)
 
-(display (foo 88))
+(display (foo 88)) ;; 88
 (newline)
 
-(display (foo 4))
+(display (foo 4)) ;; 4
 (newline)
 
-(display x)
+(display x) ;; 34
 (newline)
 
+
+(display (letrec ((foo (lambda (arg)
+					  (or arg (and (procedure? foo)
+						       (foo 99))))))
+			    (define bar (foo #f))
+			    (foo #f)))
+(newline)
+
+
+(display (letrec ((foo 77)
+				   (bar #f)
+				   (retfoo (lambda () foo)))
+			    (define baz (retfoo))
+			    (retfoo)))
+(newline)
 
 ;;;options: -l r4rs
 ;;;expected:
@@ -47,7 +67,10 @@
 ;;;1
 ;;;(3 6)
 ;;;9
-;;;34
-;;;5
 ;;;5
 ;;;34
+;;;88
+;;;4
+;;;34
+;;;99
+;;;77
