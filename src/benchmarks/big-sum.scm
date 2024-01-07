dd78b6ef5fca83bb82b14e6bb723c9b3c578458c
leo-ard <lool4516@gmail.com>
2024-01-07 13:25:03 -0500

    Remove unused benchmark folder

--- src/benchmarks/big-sum.scm
+++
@@ -1,9 +0,0 @@
-(define range (vector->list (make-vector-fill 5000 1)))
-
-(let loop ((total 0) (rest range))
-  (if (pair? rest)
-    (loop (+ total (car rest)) (cdr rest))
-    total))
-
-;;;options: -l r4rs; -l r4rs/full-r4rs
-;;;expected:
