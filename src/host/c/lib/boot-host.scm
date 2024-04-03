1d51a752b1ed9d036ffdcf39084a65bb6aaa8881
leo-ard <lool4516@gmail.com>
2024-04-03 16:41:39 -0400

    Support the rvm c host for bootstrap
    * Fix bug with gc and the c rvm
    * Add CHECK_ACCESS preprocessor variable that allows to check if pointers accessed with CAR, CDR and TAG are in the valid range
    * Add supporting functions for the bootstrap in c

---
+++ src/host/c/lib/boot-host.scm
@@ -0,0 +1,13 @@
+
+
+
+(define-primitive (command-line)
+  (use list2scm argv)
+  "{push2(list2scm(argv, argc), PAIR_TAG);break;}")
+
+;(define-primitive (file-exists? file)
+;  (use bool2scm scm2str js/node/fs)
+;  "prim1((filename) => bool2scm(fs.existsSync(scm2str(filename)))),")
+
+(define (file-exists? file)
+  (not (not (##get-fd-input-file file))))
