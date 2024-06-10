8a1ba05102e98e48c127663cd65a2dfa66ba0962
leo-ard <lool4516@gmail.com>
2024-06-10 15:54:00 -0400

    Fix apply in python rvm

--- src/lib/r4rs/control.scm
+++ src/lib/r4rs/control.scm
@@ -21,18 +21,24 @@
      }, "))
 
   ((host py)
+   (define-feature
+     ##apply
+     (decl
+"def prim_apply():
+ _arg = pop()
+ f = pop()
+ num_args=0
+ while _arg is not NIL:
+  push(_arg[0])
+  _arg=_arg[1]
+  num_args += 1
+ push(num_args) # @@(feature arity-check)@@
+ return f
+
+"))
    (define-primitive
      (##apply f args)
-     "lambda: [exec(compile(\"\"\"
-_arg = pop()
-globals()['_f_'] = pop()
-num_args=0
-while _arg is not NIL:
- push(_arg[0])
- _arg=_arg[1]
- num_args += 1
-push(num_args) # @@(feature arity-check)@@
- \"\"\", __file__, 'exec')), _f_][1],"))
+     "prim_apply,"))
 
   ((host c)
    (define-primitive
