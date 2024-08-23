ce3506ca065c520ea0b2f07ac3c6d8b8893ce4b0
leo-ard <lool4516@gmail.com>
2024-08-23 14:02:43 -0400

    Fix max-tc library and add it to tests

--- src/tests/00-ribbit/22-equalp.scm
+++ src/tests/00-ribbit/22-equalp.scm
@@ -9,6 +9,7 @@
 
 ;;;run: -l min
 ;;;run: -l max
+;;;run: -l max-tc
 ;;;r4rs-run: -l r4rs
 ;;;expected:
 ;;;BBABBAB
--- src/tests/00-ribbit/35-eval.scm
+++ src/tests/00-ribbit/35-eval.scm
@@ -5,6 +5,7 @@
 
 ;;;run: -l min
 ;;;run: -l max
+;;;run: -l max-tc
 ;;;variadics-run: -l min -f+ arity-check
 ;;;variadics-run: -l max -f+ arity-check
 ;;;r4rs-run: -l r4rs
