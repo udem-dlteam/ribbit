39308fab1ed5b4cb8e62eefc12eed58f3c825d9a
Mathis Laroche <ecomath360@gmail.com>
2025-01-15 12:39:27 -0500

    MOD - changed the display when no source files to an error

--- src/rsc.scm
+++ src/rsc.scm
@@ -5417,7 +5417,7 @@ The output is written to output.c, with an executable compiled to run-output.exe
     (if (not src-path)
 
       (begin
-        (display "*** a Scheme source file must be specified\n")
+        (error "*** a Scheme source file must be specified\n")
         (exit-program-abnormally))
 
       (fancy-compiler
