12926b3ae095396fa484096c1d4c0a25da8e39a5
lahaie <93234911+lahaief@users.noreply.github.com>
2025-01-18 15:57:04 -0500

    ES: Fixed apply primitive

--- src/host/c/lib/prim-apply.scm
+++ src/host/c/lib/prim-apply.scm
@@ -1,12 +1,10 @@
+(if-feature host/c/es.c
 
-;; FIXME segfaults on tests that don't use the ES garbage collector
-;; but I'm not sure about the syntax of the feature expansion
-
-;; ref count or even-shiloach gc
-(define-primitive
-  (##apply f args)
-  (use es-apply)
-  "{
+  ;; ref count or even-shiloach gc
+  (define-primitive
+    (##apply f args)
+    (use es-apply)
+    "{
   PRIM2();
   TEMP4 = x; // protect x 
   add_ref(NIL, x, 1);
@@ -24,47 +22,22 @@
   return TAG_RIB(x);
 }")
 
-
-;; (if-feature host/c/es.c
-
-;;   ;; ref count or even-shiloach gc
-;;   (define-primitive
-;;     (##apply f args)
-;;     "{
-;;   PRIM2();
-;;   TEMP4 = x; // protect x 
-;;   add_ref(NIL, x, 1);
-;;   int num_args = 0;
-;;   obj arg = TAG_RIB(y);
-;;   while (arg != NIL) {
-;;     push2(arg, PAIR_TAG); 
-;;     arg = CAR(stack);
-;;     SET_CAR(stack, CAR(arg));
-;;     arg = TAG_RIB(CDR(arg));
-;;     num_args++;
-;;   }
-;;   push2(TAG_NUM(num_args), PAIR_TAG);
-;;   DEC_PRIM2();
-;;   return TAG_RIB(x);
-;; }")
-
-;;   ;; mark-and-sweep or stop-and-copy gc
-;;   (define-primitive
-;;     (##apply f args)
-;;       "{
-;;   PRIM2();
-;;   TEMP1 = x; // save x for the gc 
-;;   int num_args = 0;
-;;   obj arg = TAG_RIB(y);
-;;   while (arg != NIL) {
-;;     push2(arg, PAIR_TAG); // make sure the arg doesn't get GC'd
-;;     arg = CAR(stack);
-;;     CAR(stack) = CAR(arg);
-;;     arg = TAG_RIB(CDR(arg));
-;;     num_args++;
-;;   }
-;;   push2(TAG_NUM(num_args), PAIR_TAG);
-;;   x = TEMP1; // retrive x from possibly GC'd 
-;;   TEMP1 = TAG_NUM(0); // memory leak otherwise
-;;   return TAG_RIB(x);
-;; }"))
+  ;; mark-and-sweep or stop-and-copy gc
+  (define-primitive
+    (##apply f args)
+      "{
+  PRIM2();
+  TEMP1 = x; // save x for the gc 
+  int num_args = 0;
+  obj arg = TAG_RIB(y);
+  while (arg != NIL) {
+    push2(arg, PAIR_TAG); // make sure the arg doesn't get GC'd
+    arg = CAR(stack);
+    CAR(stack) = CAR(arg);
+    arg = TAG_RIB(CDR(arg));
+    num_args++;
+  }
+  push2(TAG_NUM(num_args), PAIR_TAG);
+  x = TEMP1; // retrive x from possibly GC'd 
+  return TAG_RIB(x);
+}"))
