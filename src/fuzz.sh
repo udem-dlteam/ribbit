2362e00d0245a412b8a4280aa739cd532f844d09
leo-ard <lool4516@gmail.com>
2025-01-31 18:32:40 -0500

    update fuzz.sh

--- src/fuzz.sh
+++ src/fuzz.sh
@@ -1,42 +1,74 @@
-
-if [ "$1" == "" ]; then
-  echo "Usage: $0 <number of iterations>"
+if [ "$1" == "" ] || [ "$2" == "" ]; then
+  echo "Usage: $0 <number of iterations> <graph_size>"
   exit 1
 fi
 
-
-function run_fuzz {
-  mkdir -p .fuzz
-  mkdir -p .fuzz/errors
-  echo "===== Generating random program"
-  python3 ./host/c/gen_graph/gen.py 250 > .fuzz/temp.scm
-  echo "===== Compiling random program"
-  ./rsc.exe --rvm ./host/c/es.c -t c -l test-es -f+ c/gc/es -x run-test .fuzz/temp.scm
-  echo "===== Running random program"
-  ./run-test > .fuzz/result.txt
-  if [ "$?" != "0" ]; then \
-    echo "===== ERROR"; \
-  	n=`ls .fuzz/errors | wc -l`; \
-  	n=$(($n + 1)); \
-  	cp .fuzz/temp.scm .fuzz/errors/$n.scm; \
-  	cp .fuzz/result.scm .fuzz/errors/$n.out; \
-    exit 1; \
+function compile_and_run {
+  echo "===== Compiling & Running with '$@'"
+  ./rsc.exe --rvm ./host/c/es.c -t c -l test-es -f+ c/gc/es -x run-test $@
+  $(./run-test > .fuzz/out)
+  if [ "$?" != "0" ]; then
+    echo "===== ERROR while running program ";
+  	n=`ls .fuzz/errors | wc -l`;
+  	n=$(($n + 1));
+    echo "writing to .fuzz/errors/$n.*";
+  	cp .fuzz/run.scm .fuzz/errors/$n.scm;
+  	cp .fuzz/out .fuzz/errors/$n.out;
+  	cp .fuzz/run.scm.G.dot .fuzz/errors/$n.G.dot;
+  	cp .fuzz/run.scm.T.dot .fuzz/errors/$n.T.dot;
+    return 1;
   fi
-  cat .fuzz/result.txt | sed "s/\*\*\*REMAINING_RIBS = 0//g" | sed "s/\*\*\*ALIVE_BUT_COLLECTED = 0//g" | grep "\*\*\*"
-  if [ "$?" == "0" ]; then \
-    echo "===== ERROR"; \
-    n=`ls .fuzz/errors | wc -l`; \
-    n=$(($n + 1)); \
-    cp .fuzz/temp.scm .fuzz/errors/$n.scm; \
-    cp .fuzz/result.scm .fuzz/errors/$n.out; \
-    exit 1; \
+  cat .fuzz/out | sed "s/\*\*\*REMAINING_RIBS = 0//g" | sed "s/\*\*\*ALIVE_BUT_COLLECTED = 0//g" | grep "\*\*\*"
+  if [ "$?" == "0" ]; then
+    echo "===== ERROR didn't collect all ribs";
+    n=`ls .fuzz/errors | wc -l`;
+    n=$(($n + 1));
+    echo "writing to .fuzz/errors/$n.*";
+  	cp .fuzz/run.scm .fuzz/errors/$n.scm;
+  	cp .fuzz/out .fuzz/errors/$n.out;
+  	cp .fuzz/run.scm.G.dot .fuzz/errors/$n.G.dot;
+  	cp .fuzz/run.scm.T.dot .fuzz/errors/$n.T.dot;
+    return 1;
   fi
 }
 
+function run_fuzz {
+  graph_size=$1
+  mkdir -p .fuzz
+  mkdir -p .fuzz/errors
+  echo "===== Generating random program"
+  python3 ./host/c/gen_graph/gen.py $graph_size .fuzz/run.scm
+  compile_and_run -f+ gc/c/es
+  compile_and_run -f+ gc/c/es -f+ queue-no-remove
+  compile_and_run -f+ gc/c/es -f+ linked-list
+  compile_and_run -f+ gc/c/es  -f+ no-adopt
+  compile_and_run -f+ gc/c/es -f+ queue-no-remove -f+ no-adopt
+  compile_and_run -f+ gc/c/es -f+ linked-list -f+ no-adopt
+  compile_and_run -f+ gc/c/es  -f+ es-roots
+  compile_and_run -f+ gc/c/es -f+ queue-no-remove -f+ es-roots
+  compile_and_run -f+ gc/c/es -f+ linked-list -f+ es-roots
+  compile_and_run -f+ update-ranks -f+ gc/c/es
+  compile_and_run -f+ update-ranks -f+ gc/c/es -f+ queue-no-remove
+  compile_and_run -f+ update-ranks -f+ gc/c/es -f+ linked-list
+  compile_and_run -f+ update-ranks -f+ gc/c/es  -f+ no-adopt
+  compile_and_run -f+ update-ranks -f+ gc/c/es -f+ queue-no-remove -f+ no-adopt
+  compile_and_run -f+ update-ranks -f+ gc/c/es -f+ linked-list -f+ no-adopt
+  compile_and_run -f+ update-ranks -f+ gc/c/es  -f+ es-roots
+  compile_and_run -f+ update-ranks -f+ gc/c/es -f+ queue-no-remove -f+ es-roots
+  compile_and_run -f+ update-ranks -f+ gc/c/es -f+ linked-list -f+ es-roots
 
-for i in {1..10}; do
-  echo "===== $i"
-  run_fuzz
+}
+
+num_iterations=$1
+graph_size=$2
+i=$((num_iterations - 1))
+echo "$i"
+iterations=0
+while [ $i -ne 0 ]; do
+  echo "===== $iterations"
+  run_fuzz $graph_size
+  i=$((i - 1))
+  iterations=$((iterations + 1))
 done
 
 
