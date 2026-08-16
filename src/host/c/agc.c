0e822a6670cdf8904514947ee92c0b15e0174873
lahaie <93234911+lahaief@users.noreply.github.com>
2026-08-15 21:51:52 -0400

    AGC: update features in make check

--- src/host/c/agc.c
+++ src/host/c/agc.c
@@ -2523,7 +2523,7 @@ void run() { // evaluator
       break;
     }
     case INSTR_HALT: { // halt
-      // gc();
+      gc();
 #ifdef MIN_HEAP_SIZE
       min_nb_objects++; // null rib
       num min_heap_size = min_nb_objects * RIB_NB_FIELDS * 8;
